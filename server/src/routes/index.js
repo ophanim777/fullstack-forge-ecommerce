2b03300b9b853dfaea88964788e769655643fccf
ophanim777 <rikyjo49@gmail.com>
2026-07-26 11:00:45 +0700

    feat(server): add follow routes to index

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,6 +5,7 @@ import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
 import postRoutes from "./post.routes.js";
 import commentRoutes from "./comment.routes.js";
+import followRoutes from "./follow.routes.js";
 
 
 const router = Router();
@@ -14,5 +15,6 @@ router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use(commentRoutes);
+router.use(followRoutes);
 
 export default router;
