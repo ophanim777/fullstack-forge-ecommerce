14ac692ebc062c2da5725d8c312fb60edb351fa7
ophanim777 <rikyjo49@gmail.com>
2026-07-26 19:38:24 +0700

    feat(server): add swagger comment

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -4,6 +4,49 @@ import { authenticate } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register user baru
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - firstName
+ *               - lastName
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 example: Ricky
+ *               lastName:
+ *                 type: string
+ *                 example: Silaban
+ *               username:
+ *                 type: string
+ *                 example: ricky97
+ *               email:
+ *                 type: string
+ *                 example: ricky@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: password123
+ *     responses:
+ *       201:
+ *         description: Register berhasil
+ *       400:
+ *         description: Data tidak valid
+ */
+
 router.post("/register", register);
 router.post("/login", login);
 router.post("/refresh", refresh);
