91477e597fd8fd83164466b0b4eeb3d3d2d58383
ophanim777 <rikyjo49@gmail.com>
2026-07-26 20:11:06 +0700

    feat(server): add swagger create comment documentation

--- server/src/routes/comment.routes.js
+++ server/src/routes/comment.routes.js
@@ -4,6 +4,43 @@ import { createComment, getCommentsByPost, updateCommentById, deleteCommentById,
 
 const router = Router();
 
+
+/**
+ * @swagger
+ * /posts/{id}/comments:
+ *   post:
+ *     summary: Membuat komentar pada post
+ *     tags:
+ *       - Comments
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         example: cmrrz0wku0000ssyn9v01kozu
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - content
+ *             properties:
+ *               content:
+ *                 type: string
+ *                 example: Komentar pertama saya.
+ *     responses:
+ *       201:
+ *         description: Komentar berhasil dibuat
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Post tidak ditemukan
+ */
 router.post("/posts/:id/comments", authenticate, createComment);
 
 router.get(
