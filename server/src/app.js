e2917c3adab909126b64a45c943b8780db053ad6
ophanim777 <rikyjo49@gmail.com>
2026-07-26 19:32:19 +0700

    feat(server): add swagger to express

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger.js";
 
 import routes from "./routes/index.js";
 import { errorHandler } from "./middleware/error.middleware.js";
@@ -16,6 +18,11 @@ app.use(express.json());
 app.use("/uploads", express.static(path.resolve("uploads")));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec)
+);
 
 app.use("/api", routes);
 app.use(errorHandler);
