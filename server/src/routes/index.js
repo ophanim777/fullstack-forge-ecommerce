7d1ac48852b3695a0910be09dab4f055d3de4dbe
ophanim777 <rikyjo49@gmail.com>
2026-07-26 11:29:01 +0700

    feat(server): add get feed routes to index

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,6 +6,7 @@ import userRoutes from "./user.routes.js";
 import postRoutes from "./post.routes.js";
 import commentRoutes from "./comment.routes.js";
 import followRoutes from "./follow.routes.js";
+import feedRoutes from "./feed.routes.js";
 
 
 const router = Router();
@@ -16,5 +17,6 @@ router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use(commentRoutes);
 router.use(followRoutes);
+router.use(feedRoutes);
 
 export default router;
