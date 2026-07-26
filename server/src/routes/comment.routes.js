524f7c1d409212155325eb2dbfe43081f6375026
ophanim777 <rikyjo49@gmail.com>
2026-07-26 10:32:03 +0700

    feat(server): add get comment routes

--- server/src/routes/comment.routes.js
+++ server/src/routes/comment.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
-import { createComment } from "../controllers/comment.controller.js";
+import { createComment, getCommentsByPost, } from "../controllers/comment.controller.js";
 
 const router = Router();
 
 router.post("/posts/:id/comments", authenticate, createComment);
+
+router.get(
+  "/posts/:id/comments",
+  getCommentsByPost
+);
 
 export default router;
