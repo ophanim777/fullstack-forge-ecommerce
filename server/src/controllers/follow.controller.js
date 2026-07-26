567a4fae9729e20f57fe7f9cdbb8147050248d07
ophanim777 <rikyjo49@gmail.com>
2026-07-26 11:13:43 +0700

    feat(server): add get followers and get following controllers

--- server/src/controllers/follow.controller.js
+++ server/src/controllers/follow.controller.js
@@ -1,4 +1,5 @@
-import { toggleFollow } from "../services/follow.service.js";
+import { toggleFollow, getFollowers,
+  getFollowing, } from "../services/follow.service.js";
 
 export async function followUser(req, res, next) {
   try {
@@ -13,6 +14,34 @@ export async function followUser(req, res, next) {
         ? "Berhasil mengikuti user."
         : "Berhasil berhenti mengikuti user.",
       following: result.following,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getFollowersByUser(req, res, next) {
+  try {
+    const followers = await getFollowers(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      count: followers.length,
+      followers,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getFollowingByUser(req, res, next) {
+  try {
+    const following = await getFollowing(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      count: following.length,
+      following,
     });
   } catch (error) {
     next(error);
