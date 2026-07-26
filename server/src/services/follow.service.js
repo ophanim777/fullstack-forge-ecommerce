169d6055be5d845d6d18ee5c53a9dc5a17f93815
ophanim777 <rikyjo49@gmail.com>
2026-07-26 10:56:22 +0700

    feat(server): add follow function

---
+++ server/src/services/follow.service.js
@@ -0,0 +1,53 @@
+import { prisma } from "../config/prisma.js";
+import { ApiError } from "../utils/apiError.js";
+
+export async function toggleFollow(targetUserId, currentUserId) {
+  if (targetUserId === currentUserId) {
+    throw new ApiError(400, "Tidak dapat mengikuti akun sendiri.");
+  }
+
+  const targetUser = await prisma.user.findUnique({
+    where: {
+      id: targetUserId,
+    },
+  });
+
+  if (!targetUser) {
+    throw new ApiError(404, "User tidak ditemukan.");
+  }
+
+  const existingFollow = await prisma.follow.findUnique({
+    where: {
+      followerId_followingId: {
+        followerId: currentUserId,
+        followingId: targetUserId,
+      },
+    },
+  });
+
+  if (existingFollow) {
+    await prisma.follow.delete({
+      where: {
+        followerId_followingId: {
+          followerId: currentUserId,
+          followingId: targetUserId,
+        },
+      },
+    });
+
+    return {
+      following: false,
+    };
+  }
+
+  await prisma.follow.create({
+    data: {
+      followerId: currentUserId,
+      followingId: targetUserId,
+    },
+  });
+
+  return {
+    following: true,
+  };
+}
