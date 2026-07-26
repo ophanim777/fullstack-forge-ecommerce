5069347f9a7201f82b31fee2353678b9043416e8
ophanim777 <rikyjo49@gmail.com>
2026-07-26 10:24:55 +0700

    feat(server): add comment routes to index

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,6 +4,7 @@ import healthRoutes from "./health.routes.js";
 import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
 import postRoutes from "./post.routes.js";
+import commentRoutes from "./comment.routes.js";
 
 
 const router = Router();
@@ -12,5 +13,6 @@ router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
+router.use(commentRoutes);
 
 export default router;
