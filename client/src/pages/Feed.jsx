85441e04928274a34577846c97b79e0cded0e623
ophanim777 <rikyjo49@gmail.com>
2026-08-02 22:21:22 +0700

    feat(client): add load post setting to feed

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+
 import MainLayout from "../layouts/MainLayout";
+import * as postService from "../services/post.service";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  async function loadPosts() {
+    try {
+      const response = await postService.getPosts();
+
+      setPosts(response.posts);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <MainLayout>
-      <h1 className="text-3xl font-bold">
+      <h1 className="text-3xl font-bold mb-6">
         Feed
       </h1>
+
+      <pre>
+        {JSON.stringify(posts, null, 2)}
+      </pre>
     </MainLayout>
   );
 }
