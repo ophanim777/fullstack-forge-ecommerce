80e436ca153b18556fff035e00375425cac4b6c1
ophanim777 <rikyjo49@gmail.com>
2026-08-02 15:35:16 +0700

    feat(client): add authprovider setting for login and logout

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,15 +1,31 @@
 import { createContext, useContext, useState } from "react";
+import * as authService from "../services/auth.service";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
 
+  async function login(data) {
+    const response = await authService.login(data);
+
+    setUser(response.user);
+
+    return response;
+  }
+
+  async function logout() {
+    await authService.logout();
+
+    setUser(null);
+  }
+
   return (
     <AuthContext.Provider
       value={{
         user,
-        setUser,
+        login,
+        logout,
       }}
     >
       {children}
