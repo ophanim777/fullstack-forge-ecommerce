cd2901276dbdc94348e7ba364761c0fb9fa85d39
ophanim777 <rikyjo49@gmail.com>
2026-08-02 15:36:19 +0700

    feat(client): add login page setup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,83 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useAuth } from "../context/AuthContext";
+
 export default function Login() {
-  return <h1>Login Page</h1>;
+  const navigate = useNavigate();
+
+  const { login } = useAuth();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      await login(form);
+
+      alert("Login berhasil!");
+
+      navigate("/");
+    } catch (err) {
+      alert(
+        err.response?.data?.message ||
+          "Login gagal"
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-slate-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-xl rounded-xl p-8 w-[380px]"
+      >
+        <h1 className="text-3xl font-bold mb-6">
+          Login
+        </h1>
+
+        <input
+          className="border w-full p-3 rounded mb-4"
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) =>
+            setForm({
+              ...form,
+              email: e.target.value,
+            })
+          }
+        />
+
+        <input
+          type="password"
+          className="border w-full p-3 rounded mb-6"
+          placeholder="Password"
+          value={form.password}
+          onChange={(e) =>
+            setForm({
+              ...form,
+              password: e.target.value,
+            })
+          }
+        />
+
+        <button
+          disabled={loading}
+          className="bg-blue-600 text-white w-full py-3 rounded-lg"
+        >
+          {loading ? "Loading..." : "Login"}
+        </button>
+      </form>
+    </div>
+  );
 }
