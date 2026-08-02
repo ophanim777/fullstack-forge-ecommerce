36e06a59481ca5269a2012f00b2cf5ac786d1b48
ophanim777 <rikyjo49@gmail.com>
2026-08-02 21:41:25 +0700

    feat(client): add useeffect to authcontext

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect, } from "react";
 import * as authService from "../services/auth.service";
 
 const AuthContext = createContext();
