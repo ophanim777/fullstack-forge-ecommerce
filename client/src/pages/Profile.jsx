5c8505806b5827765b6c1e9805117e24fc606937
ophanim777 <rikyjo49@gmail.com>
2026-08-09 23:09:02 +0700

    feat(client): add open profile from followers/following list function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -556,8 +556,16 @@ async function loadFollowing() {
                           </div>
                         )}
 
-                        <div>
-                          <p className="font-semibold">
+                        <div
+                          className="cursor-pointer"
+                          onClick={() => {
+                            setShowFollowers(false);
+                            setShowFollowing(false);
+
+                            window.location.href = `/profile/${person.username}`;
+                          }}
+                        >
+                          <p className="font-semibold hover:text-blue-600">
                             {person.firstName} {person.lastName}
                           </p>
 
