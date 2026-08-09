a3c4730872dab48ebba3219ddd96e6e7ee945295
ophanim777 <rikyjo49@gmail.com>
2026-08-09 21:31:17 +0700

    feat(client): add get user by id

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -29,3 +29,21 @@ export async function uploadAvatar(file) {
 
   return response.data;
 }
+
+export async function getUserById(userId) {
+  return prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      username: true,
+      email: true,
+      bio: true,
+      avatar: true,
+      role: true,
+    },
+  });
+}
