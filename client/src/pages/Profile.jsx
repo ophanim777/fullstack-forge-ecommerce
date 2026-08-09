8d6f274d2d47533f9ccae6be3a9a583015d346a1
ophanim777 <rikyjo49@gmail.com>
2026-08-09 22:43:09 +0700

    fix(client): delete unnecessary function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { getUserProfile, updateProfile, uploadAvatar, toggleFollow,
-  getFollowing, } from "../services/user.service";
+  getFollowing, getFollowers, } from "../services/user.service";
 import { useAuth } from "../context/AuthContext";
 
 export default function Profile() {
