83610fb1e1958534f2ead0fb63cfb2f88a1ea826
ophanim777 <rikyjo49@gmail.com>
2026-08-09 19:36:11 +0700

    feat(server): add GET routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -3,8 +3,14 @@ import { authenticate } from "../middleware/auth.middleware.js";
 import { updateUserProfile } from "../controllers/user.controller.js";
 import { upload } from "../middleware/upload.middleware.js";
 import { uploadAvatar } from "../controllers/user.controller.js";
+import { getUserProfile } from "../controllers/user.controller.js";
 
 const router = Router();
+
+router.get(
+  "/profile/:username",
+  getUserProfile
+);
 
 router.put("/profile", authenticate, updateUserProfile);
 
