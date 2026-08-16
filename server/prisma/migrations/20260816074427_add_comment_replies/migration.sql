44fb2ed2888eac035efd7b9dcf02cb211db5373a
ophanim777 <rikyjo49@gmail.com>
2026-08-16 14:45:53 +0700

    feat(server): migrate schema

---
+++ server/prisma/migrations/20260816074427_add_comment_replies/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Comment" ADD COLUMN     "parentId" TEXT;
+
+-- CreateIndex
+CREATE INDEX "Comment_postId_idx" ON "Comment"("postId");
+
+-- CreateIndex
+CREATE INDEX "Comment_parentId_idx" ON "Comment"("parentId");
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
