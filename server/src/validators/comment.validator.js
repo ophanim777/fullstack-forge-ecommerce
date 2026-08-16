6205c6b7819606817d610435e79cf16feff1c28b
ophanim777 <rikyjo49@gmail.com>
2026-08-16 14:51:55 +0700

    feat(server): add parent Id to create comment schema

--- server/src/validators/comment.validator.js
+++ server/src/validators/comment.validator.js
@@ -6,6 +6,11 @@ export const createCommentSchema = z.object({
     .trim()
     .min(1, "Komentar tidak boleh kosong.")
     .max(500, "Komentar maksimal 500 karakter."),
+
+    parentId: z
+    .string()
+    .optional()
+    .nullable(),
 });
 
 export const updateCommentSchema = z.object({
