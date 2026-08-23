c6574731cb5cd9c3955fa77a1bf3783e49050778
ophanim777 <rikyjo49@gmail.com>
2026-08-23 20:12:36 +0700

    fix(server): add notification bell to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,6 +32,8 @@ export default function Navbar() {
               {user.firstName}
             </Link>
 
+            <NotificationBell />
+
             <button
               onClick={handleLogout}
               className="bg-red-500 text-white px-4 py-2 rounded-lg"
