059c2511d563293945ef3deb7057e14683c3205d
ophanim777 <rikyjo49@gmail.com>
2026-08-23 20:21:50 +0700

    feat(server): add notification read to routes

--- server/src/routes/notification.routes.js
+++ server/src/routes/notification.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
 import {
   getUserNotifications,
+  markNotificationAsRead,
 } from "../controllers/notification.controller.js";
 
 const router = Router();
@@ -10,6 +11,12 @@ router.get(
   "/notifications",
   authenticate,
   getUserNotifications
+);
+
+router.patch(
+  "/notifications/:id/read",
+  authenticate,
+  markNotificationAsRead
 );
 
 export default router;
