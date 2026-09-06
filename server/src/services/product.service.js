adb4fb957db77c5e096c1a29ea3c5b7c4039576f
ophanim777 <rikyjo49@gmail.com>
2026-09-06 20:34:11 +0700

    feat(server): add create product function to product service

--- server/src/services/product.service.js
+++ server/src/services/product.service.js
@@ -11,3 +11,20 @@ export async function getProducts() {
     },
   });
 }
+
+export async function createProduct(data) {
+  return prisma.product.create({
+    data: {
+      name: data.name.trim(),
+      slug: data.slug.trim(),
+      description: data.description.trim(),
+      price: data.price,
+      stock: Number(data.stock),
+      categoryId: data.categoryId,
+    },
+    include: {
+      category: true,
+      images: true,
+    },
+  });
+}
