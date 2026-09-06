d99f156663ff948e60935cbfd24e186bccb092bf
ophanim777 <rikyjo49@gmail.com>
2026-09-06 20:39:59 +0700

    feat(server): add controller post route to product routes

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 
-import { getProductsController } from "../controllers/product.controller.js";
+import {
+  getProductsController,
+  createProductController,
+} from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.get("/", getProductsController);
+
+router.post("/", createProductController);
 
 export default router;
