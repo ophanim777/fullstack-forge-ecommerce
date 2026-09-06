2531912e72717d1ae45eb4685edbae92238dfe8a
ophanim777 <rikyjo49@gmail.com>
2026-09-06 20:24:49 +0700

    feat(server): add product routes

---
+++ server/src/routes/product.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { getProductsController } from "../controllers/product.controller.js";
+
+const router = Router();
+
+router.get("/", getProductsController);
+
+export default router;
