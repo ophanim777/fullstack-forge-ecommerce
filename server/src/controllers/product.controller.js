3d9069e5615011508356859e03433eff6cb304d9
ophanim777 <rikyjo49@gmail.com>
2026-09-06 23:20:07 +0700

    feat(server): add get product by id controller function to product controller

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -15,6 +15,26 @@ export async function getProductsController(req, res, next) {
   }
 }
 
+export async function getProductByIdController(req, res, next) {
+  try {
+    const product = await getProductById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product tidak ditemukan.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
 export async function createProductController(req, res, next) {
   try {
     const { isValid, errors } = validateCreateProduct(req.body);
