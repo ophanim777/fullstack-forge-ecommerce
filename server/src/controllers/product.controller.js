d4c8f2e229c733a5176ac5f8e3bf0de3a5a184ec
ophanim777 <rikyjo49@gmail.com>
2026-09-06 23:27:05 +0700

    feat(server): add update product controller function to product controller

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -56,3 +56,28 @@ export async function createProductController(req, res, next) {
     next(error);
   }
 }
+
+export async function updateProductController(req, res, next) {
+  try {
+    const { isValid, errors } = validateCreateProduct(req.body);
+
+    if (!isValid) {
+      return res.status(400).json({
+        success: false,
+        errors,
+      });
+    }
+
+    const product = await updateProduct(
+      req.params.id,
+      req.body
+    );
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
