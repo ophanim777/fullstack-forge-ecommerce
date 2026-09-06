bf108dcb94229377db84e46523ba774690622151
ophanim777 <rikyjo49@gmail.com>
2026-09-06 23:23:39 +0700

    feat(server): add get product by id controller routes to product routes

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import {
   getProductsController,
   createProductController,
+  getProductByIdController,
 } from "../controllers/product.controller.js";
 
 import { authenticate } from "../middleware/auth.middleware.js";
@@ -11,6 +12,8 @@ import { requireAdmin } from "../middleware/admin.middleware.js";
 const router = Router();
 
 router.get("/", getProductsController);
+
+router.get("/:id", getProductByIdController);
 
 router.post(
   "/",
