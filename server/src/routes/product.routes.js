2a6c10020585f70c03ad75b972934614a04f7892
ophanim777 <rikyjo49@gmail.com>
2026-09-06 23:31:06 +0700

    feat(server): add update product controller function to product routes

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -4,6 +4,7 @@ import {
   getProductsController,
   createProductController,
   getProductByIdController,
+  updateProductController,
 } from "../controllers/product.controller.js";
 
 import { authenticate } from "../middleware/auth.middleware.js";
@@ -20,6 +21,13 @@ router.post(
   authenticate,
   requireAdmin,
   createProductController
+);
+
+router.patch(
+  "/:id",
+  authenticate,
+  requireAdmin,
+  updateProductController
 );
 
 export default router;
