87250648d983254a9d8bab9ec3e469ace9a2eca4
ophanim777 <rikyjo49@gmail.com>
2026-09-13 21:40:13 +0700

    feat(server): add delete category controller routes to category routes

--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -7,22 +7,32 @@ import {
   getCategoryByIdController,
   createCategoryController,
   updateCategoryController,
+  deleteCategoryController,
 } from "../controllers/category.controller.js";
 
 const router = Router();
 
 router.get("/", getCategoriesController);
 router.get("/:id", getCategoryByIdController);
+
 router.post(
   "/",
   authenticate,
   requireAdmin,
   createCategoryController
 );
+
 router.patch(
   "/:id",
   authenticate,
   requireAdmin,
   updateCategoryController
+);
+
+router.delete(
+  "/:id",
+  authenticate,
+  requireAdmin,
+  deleteCategoryController
 );
 export default router;
