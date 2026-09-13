69683ab2552401a0a1b19e226c5b06dcc41776f9
ophanim777 <rikyjo49@gmail.com>
2026-09-13 21:02:48 +0700

    feat(server): add get category by id controller routes to category routes

--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import {
   getCategoriesController,
+  getCategoryByIdController,
 } from "../controllers/category.controller.js";
 
 const router = Router();
 
 router.get("/", getCategoriesController);
+router.get("/:id", getCategoryByIdController);
 
 export default router;
