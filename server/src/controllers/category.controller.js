82765d997763bb4d7cb1d79b920e1702e3c93feb
ophanim777 <rikyjo49@gmail.com>
2026-09-13 21:00:49 +0700

    feat(server): add get category by id controller to category controller

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -1,4 +1,4 @@
-import { getCategories } from "../services/category.service.js";
+import { getCategories, getCategoryById, } from "../services/category.service.js";
 
 export async function getCategoriesController(req, res, next) {
   try {
@@ -7,6 +7,26 @@ export async function getCategoriesController(req, res, next) {
     res.status(200).json({
       success: true,
       data: categories,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getCategoryByIdController(req, res, next) {
+  try {
+    const category = await getCategoryById(req.params.id);
+
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category tidak ditemukan.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: category,
     });
   } catch (error) {
     next(error);
