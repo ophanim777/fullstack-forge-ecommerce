3e16a9c6c32dc81129cfa730d7d4eb28f498c439
ophanim777 <rikyjo49@gmail.com>
2026-09-13 21:11:00 +0700

    feat(server): add create category controller routes to category routes

--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -3,11 +3,18 @@ import { Router } from "express";
 import {
   getCategoriesController,
   getCategoryByIdController,
+  createCategoryController,
 } from "../controllers/category.controller.js";
 
 const router = Router();
 
 router.get("/", getCategoriesController);
 router.get("/:id", getCategoryByIdController);
+router.post(
+  "/",
+  authenticate,
+  requireAdmin,
+  createCategoryController
+);
 
 export default router;
