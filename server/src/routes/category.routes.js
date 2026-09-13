ef0cf9972611b4390a1634afec558969f58e47e2
ophanim777 <rikyjo49@gmail.com>
2026-09-13 21:11:54 +0700

    feat(server): import authenticate and requireadmin to category routes

--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { authenticate } from "../middleware/auth.middleware.js";
+import { requireAdmin } from "../middleware/admin.middleware.js";
 
 import {
   getCategoriesController,
