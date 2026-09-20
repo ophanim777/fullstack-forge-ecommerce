b74af0c7e86a2db7f384a075eda516dcfa329641
ophanim777 <rikyjo49@gmail.com>
2026-09-20 19:53:07 +0700

    feat(server): add add cart item controller function to cart controller

--- server/src/controllers/cart.controller.js
+++ server/src/controllers/cart.controller.js
@@ -1,7 +1,10 @@
 import {
   getCartByUserId,
   createCart,
+  addCartItem,
 } from "../services/cart.service.js";
+
+import { validateAddCartItem } from "../validators/cart.validator.js";
 
 export async function getCartController(req, res, next) {
   try {
@@ -16,6 +19,54 @@ export async function getCartController(req, res, next) {
       data: cart,
     });
   } catch (error) {
+    next(error);
+  }
+}
+
+
+export async function addCartItemController(req, res, next) {
+  try {
+    const { isValid, errors } = validateAddCartItem(req.body);
+
+    if (!isValid) {
+      return res.status(400).json({
+        success: false,
+        errors,
+      });
+    }
+
+    const item = await addCartItem(
+      req.user.id,
+      req.body.productId,
+      Number(req.body.quantity)
+    );
+
+    res.status(201).json({
+      success: true,
+      data: item,
+    });
+  } catch (error) {
+    if (error.message === "CART_NOT_FOUND") {
+      return res.status(404).json({
+        success: false,
+        message: "Cart tidak ditemukan.",
+      });
+    }
+
+    if (error.message === "PRODUCT_NOT_FOUND") {
+      return res.status(404).json({
+        success: false,
+        message: "Product tidak ditemukan.",
+      });
+    }
+
+    if (error.message === "INSUFFICIENT_STOCK") {
+      return res.status(409).json({
+        success: false,
+        message: "Stok product tidak mencukupi.",
+      });
+    }
+
     next(error);
   }
 }
