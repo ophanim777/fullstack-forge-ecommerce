9672601f6c2df67eb68d4569e2e002e9d45172ef
ophanim777 <rikyjo49@gmail.com>
2026-09-20 20:35:43 +0700

    feat(server): add validate update cart item to cart validator

--- server/src/validators/cart.validator.js
+++ server/src/validators/cart.validator.js
@@ -23,3 +23,25 @@ export function validateAddCartItem(data) {
     errors,
   };
 }
+
+export function validateUpdateCartItem(data) {
+  const errors = {};
+
+  if (
+    data.quantity === undefined ||
+    data.quantity === null ||
+    data.quantity === ""
+  ) {
+    errors.quantity = "Quantity is required";
+  } else if (
+    !Number.isInteger(Number(data.quantity)) ||
+    Number(data.quantity) < 1
+  ) {
+    errors.quantity = "Quantity must be a positive integer";
+  }
+
+  return {
+    isValid: Object.keys(errors).length === 0,
+    errors,
+  };
+}
