e0ceed38409ecebf2e43de08ed450227423798cb
ophanim777 <rikyjo49@gmail.com>
2026-09-20 21:04:41 +0700

    feat(server): add create order controller to order controller

--- server/src/controllers/order.controller.js
+++ server/src/controllers/order.controller.js
@@ -1,6 +1,7 @@
 import {
   getOrdersByUserId,
   getOrderById,
+  createOrder,
 } from "../services/order.service.js";
 
 export async function getOrdersController(req, res, next) {
@@ -35,6 +36,40 @@ export async function getOrderByIdController(req, res, next) {
       data: order,
     });
   } catch (error) {
+    next(error);
+  }
+}
+
+export async function createOrderController(req, res, next) {
+  try {
+    const order = await createOrder(req.user.id);
+
+    res.status(201).json({
+      success: true,
+      data: order,
+    });
+  } catch (error) {
+    if (error.message === "CART_NOT_FOUND") {
+      return res.status(404).json({
+        success: false,
+        message: "Cart tidak ditemukan.",
+      });
+    }
+
+    if (error.message === "CART_EMPTY") {
+      return res.status(400).json({
+        success: false,
+        message: "Cart masih kosong.",
+      });
+    }
+
+    if (error.message === "INSUFFICIENT_STOCK") {
+      return res.status(409).json({
+        success: false,
+        message: "Stok product tidak mencukupi.",
+      });
+    }
+
     next(error);
   }
 }
