edf97caf64b6382c6b785d420c820e24b78d1af9
ophanim777 <rikyjo49@gmail.com>
2026-09-20 21:22:02 +0700

    feat(server): add update order status controller routes to order routes

--- server/src/routes/order.routes.js
+++ server/src/routes/order.routes.js
@@ -3,10 +3,13 @@ import { Router } from "express";
 import {
   getOrdersController,
   getOrderByIdController,
-  createOrderController
+  createOrderController,
+  updateOrderStatusController,
 } from "../controllers/order.controller.js";
 
 import { authenticate } from "../middleware/auth.middleware.js";
+
+import { requireAdmin } from "../middleware/admin.middleware.js";
 
 const router = Router();
 
@@ -26,6 +29,13 @@ router.get(
   "/:id",
   authenticate,
   getOrderByIdController
+);
+
+router.patch(
+  "/:id/status",
+  authenticate,
+  requireAdmin,
+  updateOrderStatusController
 );
 
 export default router;
